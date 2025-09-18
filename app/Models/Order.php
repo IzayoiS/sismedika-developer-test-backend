0aebcb9aee5535197a96a01cf2c516018749fab3
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-18 20:10:05 +0700

    Add Order model and migration for managing orders

---
+++ app/Models/Order.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'table_id',
+        'user_id',
+        'status',
+        'total_price',
+        'opened_at',
+        'closed_at',
+    ];
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
