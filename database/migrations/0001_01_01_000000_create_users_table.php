939d9196c1eae15a15ddab7f6380acbcae80bfc5
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-18 20:08:36 +0700

    Add soft deletes to users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['pelayan', 'kasir'])->default('pelayan');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
