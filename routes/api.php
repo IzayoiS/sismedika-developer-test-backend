a5a3cd980f4802b0177e8e6a068c9a29d526ed5a
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-19 14:31:05 +0700

    feat: add Food and Table routes with kasir role protection

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FoodController;
+use App\Http\Controllers\Api\TableController;
 use Illuminate\Support\Facades\Route;
 
+// public
+Route::get('/foods', [FoodController::class, 'index']);
+Route::get('/foods/{food}', [FoodController::class, 'show']);
+Route::get('/tables', [TableController::class, 'index']);
+
+// auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -13,4 +21,11 @@ Route::middleware('auth:sanctum')->group(function () {
     // Route for pelayan role
     Route::middleware('role:kasir')->group(function () {
     });
+});
+
+// protected routes for kasir
+Route::middleware(['auth:sanctum', 'role:kasir'])->group(function () {
+    Route::post('/foods', [FoodController::class, 'store']);
+    Route::put('/foods/{food}', [FoodController::class, 'update']);
+    Route::delete('/foods/{food}', [FoodController::class, 'destroy']);
 });
