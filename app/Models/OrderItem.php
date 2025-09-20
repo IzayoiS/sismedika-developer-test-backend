51cc6515d4b830cd2c3d213499e4f632740d576a
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-21 03:16:01 +0700

    refactor: rename quantity field to qty in OrderItem model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -12,7 +12,7 @@ class OrderItem extends Model
     protected $fillable = [
         'order_id',
         'food_id',
-        'quantity',
+        'qty',
         'price',
         'subtotal',
     ];
