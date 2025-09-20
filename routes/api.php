312f7cbff2c9bfcd68949236ebb506e2dd3f042e
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-21 03:11:28 +0700

    feat: add order and table management API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FoodController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\TableController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,20 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 
+    // table routes
+    Route::apiResource('tables', TableController::class)->only(['index', 'store']);
+    Route::post('/tables/{id}/reserve', [TableController::class, 'reserve']);
+
+    // order routes
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::get('/orders/{id}', [OrderController::class, 'show']);
+    Route::post('/orders', [OrderController::class, 'store']);
+    Route::post('/orders/{id}/items', [OrderController::class, 'addItem']);
+
     // Route for kasir role
     Route::middleware('role:kasir')->group(function () {
+        Route::post('/tables/{id}/inactive', [TableController::class, 'setInactive']);
+        Route::put('/foods/{id}/status', [FoodController::class, 'updateStatus']);
         Route::post('/foods', [FoodController::class, 'store']);
         Route::put('/foods/{food}', [FoodController::class, 'update']);
         Route::delete('/foods/{food}', [FoodController::class, 'destroy']);
